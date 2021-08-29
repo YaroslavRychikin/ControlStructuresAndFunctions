6d01c9fb5750370d6becd1472341acfae0e24918
Yaroslav Rychikin <yaroslavrychikin@yandex.ru>
2021-08-29 21:47:48 +0400

    Corrections in the code

--- src/main/kotlin/DifferentСommission.kt
+++ src/main/kotlin/DifferentСommission.kt
@@ -12,6 +12,6 @@ fun calculationOfTheCommission(bankCard: String, amount: Int,
     "Mastercard", "Maestro" -> if (theAmountOfPreviousTransfersInThisMonth + amount <= 75000*100) "нет комисси"
         else "${(amount * 0.006 + 20 * 100).toInt()} копеек"
     "Visa", "Мир" -> if (amount * 0.0075 > 35*100) "${(amount * 0.0075).toInt()} копеек" else "${35 * 100} копеек"
-
+    "VK Pay" -> "нет комисии"
     else -> "такие карты или счета не принимаються"
 }
