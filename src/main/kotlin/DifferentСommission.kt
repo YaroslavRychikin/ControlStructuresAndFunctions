1db9fce7cd2915b5987ed5dcd670e70969d09a50
Yaroslav Rychikin <yaroslavrychikin@yandex.ru>
2021-08-29 21:44:04 +0400

    New Homework about tests

--- src/main/kotlin/DifferentСommission.kt
+++ src/main/kotlin/DifferentСommission.kt
@@ -1,16 +1,17 @@
 fun main(){
-    val amount = 750000
+    val amount = 600_000
     val bankCard = "Maestro"
-    val theAmountOfPreviousTransfersInThisMonth = 740000
+    val theAmountOfPreviousTransfersInThisMonth = 7500000
     print("Комиссия: ${calculationOfTheCommission(bankCard, amount, 
         theAmountOfPreviousTransfersInThisMonth)}")
 }
 
-fun calculationOfTheCommission(bankCard: String = "Vk Pay", amount: Int,
-                               theAmountOfPreviousTransfersInThisMonth: Int = 0)
+fun calculationOfTheCommission(bankCard: String, amount: Int,
+                               theAmountOfPreviousTransfersInThisMonth: Int)
     = when(bankCard){
     "Mastercard", "Maestro" -> if (theAmountOfPreviousTransfersInThisMonth + amount <= 75000*100) "нет комисси"
         else "${(amount * 0.006 + 20 * 100).toInt()} копеек"
     "Visa", "Мир" -> if (amount * 0.0075 > 35*100) "${(amount * 0.0075).toInt()} копеек" else "${35 * 100} копеек"
-    else -> "нет комисии"
+
+    else -> "такие карты или счета не принимаються"
 }
---
+++ src/test/kotlin/DifferentСommissionKtTest.kt
@@ -0,0 +1,106 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DifferentCommissionKtTest {
+
+    @Test
+    fun calculationOfTheCommission_noCommissionWithMaestro() {
+        val amount = 600_000
+        val bankCard = "Maestro"
+        val theAmountOfPreviousTransfersInThisMonth = 740000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("нет комисси", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_noCommissionWithMastercard() {
+        val amount = 600_000
+        val bankCard = "Mastercard"
+        val theAmountOfPreviousTransfersInThisMonth = 740000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("нет комисси", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_noCommissionWithVKPay() {
+        val amount = 600_000
+        val bankCard = "VK Pay"
+        val theAmountOfPreviousTransfersInThisMonth = 740000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("нет комисси", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_getCommissionWithMaestro() {
+        val amount = 600_000
+        val bankCard = "Maestro"
+        val theAmountOfPreviousTransfersInThisMonth = 7500000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("5600 копеек", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_getCommissionWithMastercard() {
+        val amount = 600_000
+        val bankCard = "Mastercard"
+        val theAmountOfPreviousTransfersInThisMonth = 7500000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("5600 копеек", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_getCommissionWithVisa() {
+        val amount = 600_000
+        val bankCard = "Visa"
+        val theAmountOfPreviousTransfersInThisMonth = 750000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("4500 копеек", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_getCommissionWithWorld() {
+        val amount = 600_000
+        val bankCard = "Мир"
+        val theAmountOfPreviousTransfersInThisMonth = 750000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("4500 копеек", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_theMinimumCommissionForWorld() {
+        val amount = 45_000
+        val bankCard = "Мир"
+        val theAmountOfPreviousTransfersInThisMonth = 750000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("3500 копеек", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_theMinimumCommissionForVisa() {
+        val amount = 45_000
+        val bankCard = "Visa"
+        val theAmountOfPreviousTransfersInThisMonth = 750000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("3500 копеек", result)
+    }
+
+    @Test
+    fun calculationOfTheCommission_variantElse() {
+        val amount = 600_000
+        val bankCard = "VK y"
+        val theAmountOfPreviousTransfersInThisMonth = 740000
+        val result = calculationOfTheCommission(bankCard = bankCard, amount = amount,
+            theAmountOfPreviousTransfersInThisMonth = theAmountOfPreviousTransfersInThisMonth)
+        assertEquals("такие карты или счета не принимаються", result)
+    }
+}
